e4430afeb092749c112667dfaa746970894da4ef
Shah Kibria <shahkibria@Shahs-MacBook-Air.local>
2021-04-06 22:10:57 +0600

    upload

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -38,4 +38,42 @@ Foreign key (emp_no) References employees (emp_no),
      PRIMARY KEY (emp_no)
 );
 
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE titles (
+     emp_no int NOT NULL,
+	 title varchar not null, 
+	 from_date date not null,
+	 to_date date NOT NULL,
+Foreign key (emp_no) References employees (emp_no),
+     PRIMARY KEY (emp_no)
+);
+
+DROP TABLE titles CASCADE;
+
+-- Creating tables for PH-EmployeeDB afer making from_date also a PK
+CREATE TABLE titles (
+     emp_no int NOT NULL,
+	 title varchar not null, 
+	 from_date date not null,
+	 to_date date NOT NULL,
+Foreign key (emp_no) References employees (emp_no),
+     PRIMARY KEY (emp_no, from_date)
+);
+
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE deptemp (
+     emp_no int NOT NULL,
+	 dept_no varchar(4) NOT NULL,
+	 from_date date not null,
+	 to_date date NOT NULL,
+Foreign key (dept_no) References departments (dept_no),
+Foreign key (emp_no) References employees (emp_no),
+     PRIMARY KEY (emp_no, dept_no)
+);
+
 Select * From departments ;
+Select * From deptemp ;
+Select * From employees ;
+Select * From managers ;
+Select * From salaries ;
+Select * From titles ;
